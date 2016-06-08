540f53b966bd59c1731fbed9b974b1837de4b937
Nur Yaumi Anwar <nuryaumi10@gmail.com>
2016-06-08 16:33:32 +0800

    push to github

--- bioskop.py
+++ bioskop.py
@@ -2,19 +2,17 @@ def bioskop(film, data):
     for a in data:
         for b in film:
             if a['umur'] >= b['age']:
-                watch = {"nama":"", "film":[]}
-                watch['nama'].append(a['nama'])
-                watch['film'].append(b['nama'])
+                a['film'].append(b['nama'])
 
     print film
     print data
-    print watch
+
 
 film = [{"nama": "Deadpol", "age":17}, {"nama": "AADC2", "age":17},
         {"nama": "My Stupid Boss", "age":0}, {"nama": "Traffic", "age":13},
         {"nama":"Teenage Mutan Ninja Turtles", "age":13}]
-data = [{"nama": "Budi", "umur": 25}, {"nama": "Lauri", "umur": 18},
-        {"nama": "Anton", "umur": 12}, {"nama": "Hamzah", "umur": 15},
-        {"nama": "Budi", "umur": 25}, {"nama": "Budi", "umur": 25}]
+data = [{"nama": "Budi", "umur": 25, "film":[]}, {"nama": "Lauri", "umur": 18, "film":[]},
+        {"nama": "Anton", "umur": 12, "film":[]}, {"nama": "Hamzah", "umur": 15, "film":[]},
+        {"nama": "Indra", "umur": 17, "film":[]}]
 
 bioskop(film, data)
